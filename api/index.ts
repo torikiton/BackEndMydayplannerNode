7f731113d5fe984a54cbb1a8894429703555026a
Projectmuutoo <projectmuutoo@gmail.com>
2025-03-28 23:07:15 +0700

    testtestset

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 export const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.send('Get testtest in index.ts');
+    res.send('Get testtesttest in index.ts');
 });
