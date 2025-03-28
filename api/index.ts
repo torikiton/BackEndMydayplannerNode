f52f7e2ba9fcd0e04fa1b98a06c23f78c0d8c878
Projectmuutoo <projectmuutoo@gmail.com>
2025-03-28 22:51:12 +0700

    first commit

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 export const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.send('Get test in index.ts');
+    res.send('Get testtest in index.ts');
 });
